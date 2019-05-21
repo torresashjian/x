57349a9abf76df59735ec9024d3e6148327f4f59
torresashjian <migueltorreslopez@gmail.com>
2019-05-20 20:56:29 -0700

    Add syn dependency in dovetail_derive

--- dovetail_core/src/lib.rs
+++ dovetail_core/src/lib.rs
@@ -3,16 +3,9 @@
 // in the license file that is distributed with this file.
 extern crate serde;
 extern crate serde_json;
-extern crate proc_macro2;
-
 #[macro_use]
 extern crate serde_derive;
-#[macro_use]
-extern crate quote;
-#[macro_use]
-extern crate syn;
-
-use syn::{Field, quote};
+extern crate proc_macro2;
 
 pub mod activity;
 pub mod app;
@@ -26,14 +19,4 @@ pub struct DataType {
     pub typ: String,
     #[serde(default)]
     pub value: String,
-}
- 
-impl From<DataType> for Field {
-    fn from(inner: DataType) -> Field {
-        let my_field = quote! {
-            my_field: String,
-        };
-        let input = parse_macro_input!(my_field as Field);
-        return input;
-    }
 }
